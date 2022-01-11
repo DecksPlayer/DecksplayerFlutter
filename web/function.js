33744ae8e1c2c3e06334edd0efb721e25739e855
gonojuarez <gonojuarez17@gmail.com>
2022-01-11 01:34:36 -0300

    First Version

--- web/function.js
+++
@@ -1,9 +0,0 @@
- export var firebaseConfig = {
-       apiKey: "AIzaSyCbtToumiXKGDC6c0vGLoWdNekFrzTHmV8",
-       authDomain: "decksplayer-42757.firebaseapp.com",
-       projectId: "decksplayer-42757",
-       storageBucket: "decksplayer-42757.appspot.com",
-       messagingSenderId: "1094941550793",
-       appId: "1:1094941550793:web:7e2855b8838ba9833cc518",
-       measurementId: "G-Y1S2CB099F"
-      };
